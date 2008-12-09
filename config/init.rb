939d604d2ee4f0dd3e70ef6ac2d60342f5d8253b
Matthew Ford <matt@new-bamboo.co.uk>
2008-12-09 22:17:34 +0000

    update merb to 1.0.4:

--- config/init.rb
+++ config/init.rb
@@ -11,8 +11,8 @@ Merb::Config.use do |c|
   c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
   
   # cookie session store configuration
-  c[:session_secret_key]  = 'd1f4099b91f0502673a1b63cc3f330f6c22a5f9c'  # required for cookie session store
-  c[:session_id_key] = '_comic-review_session_id' # cookie session id key, defaults to "_session_id"
+  c[:session_secret_key]  = 'b5cc483490ce7f46ce300ba7f4c87dce96dc76ad'  # required for cookie session store
+  c[:session_id_key] = '_._session_id' # cookie session id key, defaults to "_session_id"
 end
  
 Merb::BootLoader.before_app_loads do
