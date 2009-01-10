2cfba8eb8f61b20f13320e6bc8e2631a3660875b
Matthew Ford <matt@new-bamboo.co.uk>
2009-01-10 19:21:58 +0000

    homepage added passing signup feature flash messages in application template create redirect to the homepage

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -28,7 +28,7 @@ class Users < Application
   def create(user)
     @user = User.new(user)
     if @user.save
-      redirect resource(@user), :message => {:notice => "User was successfully created"}
+      redirect url(:homepage), :message => {:success => "User was successfully created"}
     else
       message[:error] = "User failed to be created"
       render :new
