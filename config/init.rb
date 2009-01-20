03f2e175ad30edaf0a56ca5117906dd46a66dc98
Matthew Ford <matt@new-bamboo.co.uk>
2009-01-20 20:47:13 +0000

    update merb and datmapper to 1.0.8.1 and 0.9.11

--- config/init.rb
+++ config/init.rb
@@ -18,6 +18,7 @@ end
 Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
   require "gravatar"
+  require "wikipedia"
 end
  
 Merb::BootLoader.after_app_loads do
