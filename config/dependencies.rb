a07e14c5821d529dede8aa7edfe75ea8e099025c
Matthew Ford <matt@new-bamboo.co.uk>
2009-02-01 12:15:05 +0000

    Add dm-tags for tagging to dependencies
    * Increase dm version to 0.9.11
    * forgot to include nokogiri
    * formatting tabs -> spaces

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
 merb_gems_version = "1.0.8.1"
-dm_gems_version   = "0.9.10"
+dm_gems_version   = "0.9.11"
 do_gems_version   = "0.9.11"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
@@ -24,10 +24,12 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
-dependency "dm-serializer", dm_gems_version   
+dependency "dm-serializer", dm_gems_version
+dependency "dm-tags", dm_gems_version   
 
 dependency "merb_datamapper", merb_gems_version
 
 # other gems & github.com gems
 dependency "jnunemaker-httparty", :require_as => "httparty"
+dependency "nokogiri"
 dependency "rdiscount"
